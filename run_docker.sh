039a15d377ca3d93869acee1c0106b603487c46f
Matt FRY <E209739@na.int.grp>
2022-09-28 13:27:22 -0400

    added simple hello world app

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=demolocal .
+docker build --tag=capstone .
 
 # Step 2: 
 # List docker images
@@ -13,4 +13,4 @@ docker image ls
 # Step 3: 
 # Run flask app
 # docker run -it demolocal bash
-docker run -p 8000:80 demolocal
+docker run -p 8000:80 capstone
